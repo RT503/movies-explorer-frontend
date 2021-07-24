ce8b3a1cd996d11f0fc25d2e074714698326aee4
RT503 <musinspiration@yandex.ru>
2021-07-24 21:36:12 +0300

    routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,32 @@
-import logo from '../../logo.svg';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="root">
+      <Switch>
+        <Route exact path="/">
+          <Main/>
+        </Route>
+        <Route>
+          <Login/>
+        </Route>
+        <Route>
+          <Register/>
+        </Route>
+        <Route>
+          <Profile/>
+        </Route>
+        <Route>
+          <Movies/>
+        </Route>
+        <Route>
+          <SavedMovies/>
+        </Route>
+        <Route path="*" />
+      </Switch>
     </div>
   );
 }
