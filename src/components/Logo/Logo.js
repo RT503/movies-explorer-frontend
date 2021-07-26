e614825bf75fa70f0417427dda5c1c7f5b61ff58
RT503 <musinspiration@yandex.ru>
2021-07-26 17:26:33 +0300

    feat: logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 import './Logo.css';
 
 function Logo() {
   return (
-
-  )
+    <Link to="/" className="logo" />
+  );
 }
+
+export default Logo;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import Logo from '../Logo/Logo';
 import './Main.css';
 
 function Main() {
   return (
     <main className="main">
       <header className="header">
-
+        <Logo/>
       </header>
     </main>
   );
