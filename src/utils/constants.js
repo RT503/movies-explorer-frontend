2a9b4665d8a08e0b89ed11d6d10c998df38e803c
RT503 <musinspiration@yandex.ru>
2021-08-16 23:36:41 +0300

    almost

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,3 +7,15 @@ export const techs = ['HTML', 'CSS', 'JS', 'React', 'Git', 'Express.js', 'mongoD
 export const staticSite = 'https://github.com/RT503/how-to-learn';
 export const adaptiveSite = 'https://github.com/RT503/russian-travel';
 export const spa = 'https://github.com/RT503/react-mesto-auth';
+
+export const fetchErrorMessage = 'Во время запроса произошла ошибка. Возможно, проблема с соединением или сервер недоступен. Подождите немного и попробуйте ещё раз';
+export const successMessage = 'Успешно!';
+export const searchErrorMessage = 'Необходимо ввести ключевое слово';
+
+export const moviesCountToRenderSmall = 5;
+export const moviesCountToRenderMedium = 8;
+export const moviesCountToRenderBig = 12;
+export const moviesCountToMoreRenderSmall = 2;
+export const moviesCountToMoreRenderBig = 3;
+
+export const shortDuration = 40;
